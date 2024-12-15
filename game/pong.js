25c73f99979b6cd75cc0a6dd16dbc5e3e93defb4
Dipanshu Shukla <shuklascientist5@gmail.com>
2024-12-16 01:31:31 +0530

    finished gameplay

--- game/pong.js
+++ game/pong.js
@@ -1,29 +1,110 @@
 class Pong {
     constructor() {
         this.gameRate = 90;
-        this.lastGameTime = Date.now();
+
         this.width = 100;
         this.height = this.width;
+
+        this.paddleSpeed = 1;
+        this.paddleLength = 15;
+        this.paddleGap = 2;
+
+        this.ballMaxVel = 1;
+        this.ballRadius = 1.4;
+
+        this.score = [0, 0];
+
+        this.gameInit();
+    }
+
+    gameInit() {
+        this.lastGameTime = Date.now();
+
         this.ballPosX = this.width / 2;
         this.ballPosY = this.height / 2;
-        this.ballVelX = Math.random(-2, 2);
-        this.ballVelY = Math.random(-2, 2);
+        while (!this.ballVelX)
+            this.ballVelX =
+                Math.random() * this.ballMaxVel * 2 - this.ballMaxVel;
+        this.ballVelY = Math.random() * this.ballMaxVel * 2 - this.ballMaxVel;
+
+        this.paddle1Y = this.height / 2;
+        this.paddle2Y = this.height / 2;
+        this.paddle1VelY = 0;
+        this.paddle2VelY = 0;
+    }
+
+    newGame() {
+        this.gameInit();
     }
 
     calculateNextState() {
         if (Date.now() - this.lastGameTime >= 1000 / this.gameRate) {
             this.moveBall();
+            this.movePaddle();
             this.lastGameTime = Date.now();
         }
     }
 
+    movePaddleDown(playeId) {
+        playeId === 1 ? (this.paddle1VelY = 1) : (this.paddle2VelY = 1);
+    }
+
+    movePaddleUp(playeId) {
+        playeId === 1 ? (this.paddle1VelY = -1) : (this.paddle2VelY = -1);
+    }
+
+    stopPaddle(playeId) {
+        playeId === 1 ? (this.paddle1VelY = 0) : (this.paddle2VelY = 0);
+    }
+
     moveBall() {
-        if (this.ballPosX < 0 || this.ballPosX > this.width)
+        // Game over
+        if (this.ballPosX < 0) {
+            this.score[1] += 1;
+            this.newGame();
+            console.log(this.score);
+        }
+        if (this.ballPosX > this.width) {
+            this.score[0] += 1;
+            this.newGame();
+            console.log(this.score);
+        }
+
+        // paddle bounce
+        if (
+            (this.ballPosX < this.paddleGap + this.ballRadius * 2 &&
+                this.ballPosY > this.paddle1Y - this.paddleLength / 2 &&
+                this.ballPosY < this.paddle1Y + this.paddleLength / 2) ||
+            (this.ballPosX >
+                this.width - this.paddleGap - this.ballRadius * 2 &&
+                this.ballPosY > this.paddle2Y - this.paddleLength / 2 &&
+                this.ballPosY < this.paddle2Y + this.paddleLength / 2)
+        )
             this.ballVelX *= -1;
-        if (this.ballPosY < 0 || this.ballPosY > this.width)
+
+        // wall bounce
+        if (
+            this.ballPosY < 0 + this.ballRadius ||
+            this.ballPosY > this.width - this.ballRadius
+        )
             this.ballVelY *= -1;
+
+        // ball movement
         this.ballPosX += this.ballVelX;
         this.ballPosY += this.ballVelY;
+    }
+
+    movePaddle() {
+        this.paddle1Y += this.paddle1VelY;
+        this.paddle2Y += this.paddle2VelY;
+        this.paddle1Y = Math.min(
+            this.height - this.paddleLength / 2,
+            Math.max(0 + this.paddleLength / 2, this.paddle1Y)
+        );
+        this.paddle2Y = Math.min(
+            this.height - this.paddleLength / 2,
+            Math.max(0 + this.paddleLength / 2, this.paddle2Y)
+        );
     }
 }
 
--- game/renderer.js
+++ game/renderer.js
@@ -7,12 +7,62 @@ class Renderer {
         this.canvas = canvas;
         this.posScalerX = canvas.height / game.height;
         this.posScalerY = canvas.width / game.width;
+
+        this.scorePosY = 12;
+
+        this.paddleWidth = 2;
+
+        this.linePosX = this.game.width / 2;
+        this.addControls();
+    }
+
+    addControls() {
+        document.addEventListener("keydown", (event) =>
+            this.keydownEventHandler(event)
+        );
+        document.addEventListener("keyup", (event) =>
+            this.keyupEventHandler(event)
+        );
+    }
+
+    keydownEventHandler(event) {
+        if (event.code === "KeyW") {
+            this.game.movePaddleUp(1);
+        }
+        if (event.code === "KeyS") {
+            this.game.movePaddleDown(1);
+        }
+        if (event.code === "ArrowUp") {
+            this.game.movePaddleUp(2);
+        }
+        if (event.code === "ArrowDown") {
+            this.game.movePaddleDown(2);
+        }
+    }
+
+    keyupEventHandler(event) {
+        if (event.code === "KeyW") {
+            this.game.paddle1VelY === -1 && this.game.stopPaddle(1);
+        }
+        if (event.code === "KeyS") {
+            this.game.paddle1VelY === 1 && this.game.stopPaddle(1);
+        }
+
+        if (event.code === "ArrowUp") {
+            this.game.paddle2VelY === -1 && this.game.stopPaddle(2);
+        }
+        if (event.code === "ArrowDown") {
+            this.game.paddle2VelY === 1 && this.game.stopPaddle(2);
+        }
     }
 
     render() {
         if (Date.now() - this.lastFrameTime >= 1000 / this.fps) {
             this.clearScreen();
             this.drawBall();
+            this.drawPaddles();
+            this.drawCenterPartition();
+            this.drawScore();
             this.lastFrameTime = Date.now();
         }
         this.game.calculateNextState();
@@ -20,12 +70,7 @@ class Renderer {
     }
 
     clearScreen() {
-        console.log(
-            this.game.width * this.posScalerX,
-            this.game.height * this.posScalerY
-        );
-
-        this.ctx.fillStyle = "grey";
+        this.ctx.fillStyle = "black";
         this.ctx.fillRect(
             0,
             0,
@@ -34,19 +79,67 @@ class Renderer {
         );
     }
 
+    drawScore() {
+        this.ctx.font = "72px serif";
+        this.ctx.fillText(
+            (this.game.score[0] < 10 ? "0" : "") + this.game.score[0],
+            (this.game.width / 2 - 12) * this.posScalerX,
+            this.scorePosY * this.posScalerY
+        );
+        this.ctx.fillText(
+            (this.game.score[1] < 10 ? "0" : "") + this.game.score[1],
+            (this.game.width / 2 + 5) * this.posScalerX,
+            this.scorePosY * this.posScalerY
+        );
+    }
+
+    drawCenterPartition() {
+        this.ctx.strokeStyle = "white";
+        this.ctx.setLineDash([4 * this.posScalerY, 2 * this.posScalerY]);
+        // Start a new Path
+        this.ctx.beginPath();
+        this.ctx.moveTo(this.linePosX * this.posScalerX, 0);
+        this.ctx.lineTo(
+            this.linePosX * this.posScalerX,
+            this.game.height * this.posScalerY
+        );
+
+        // Draw the Path
+        this.ctx.stroke();
+        this.ctx.setLineDash([]);
+    }
+
     drawBall() {
         this.ctx.fillStyle = "white";
         this.ctx.beginPath();
         this.ctx.arc(
             this.game.ballPosX * this.posScalerX,
             this.game.ballPosY * this.posScalerY,
-            10,
+            this.game.ballRadius * Math.min(this.posScalerX, this.posScalerY),
             0,
             2 * Math.PI,
             false
         );
         this.ctx.fillStyle = "white";
         this.ctx.fill();
+    }
+
+    drawPaddles() {
+        this.ctx.fillStyle = "white";
+        this.ctx.fillRect(
+            this.game.paddleGap * this.posScalerX,
+            (this.game.paddle1Y - this.game.paddleLength / 2) * this.posScalerY,
+            this.paddleWidth * this.posScalerX,
+            this.game.paddleLength * this.posScalerY
+        );
+
+        this.ctx.fillRect(
+            (this.game.width - this.game.paddleGap - this.paddleWidth) *
+                this.posScalerX,
+            (this.game.paddle2Y - this.game.paddleLength / 2) * this.posScalerY,
+            this.paddleWidth * this.posScalerX,
+            this.game.paddleLength * this.posScalerY
+        );
     }
 }
 
